2bf3927dfab24349038d75271b6431d3c79b9124
TaeBanna <sazxqinm@gmail.com>
2025-06-03 22:35:32 +0700

    Update Uiclose.lua

--- Uiclose.lua
+++ Uiclose.lua
@@ -1,23 +1,34 @@
-local ScreenGui = Instance.new("ScreenGui")
-ScreenGui.Name = "ScreenGui"
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ResetOnSpawn = false
-
+local ToggleGui = Instance.new("ScreenGui")
 local Toggle = Instance.new("TextButton")
-Toggle.Name = "Toggle"
-Toggle.Parent = ScreenGui
-Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
-Toggle.Size = UDim2.new(0, 90, 0, 38)
-Toggle.Font = Enum.Font.SourceSans
-Toggle.Text = "Tutorial"
-Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
-Toggle.TextSize = 28.000
-Toggle.Draggable = true
-Toggle.MouseButton1Click:Connect(function()
-    Library:ToggleUI()
-end)
+local UICorner = Instance.new("UICorner")
 
-local Corner = Instance.new("UICorner")
-Corner.Name = "Corner"
-Corner.Parent = Toggle
+local IsOpen = true
+
+ToggleGui.Name = "Toggle Gui"
+ToggleGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ToggleGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ToggleGui.ResetOnSpawn = false
+
+UICorner.Parent = Toggle
+Toggle.Name = "Toggle"
+Toggle.Parent = ToggleGui
+Toggle.BackgroundColor3 = Color3.fromRGB(29, 29, 29)
+Toggle.Position = UDim2.new(0, 0, 0.4547, 0)
+Toggle.Size = UDim2.new(0, 80, 0, 38)
+Toggle.Font = Enum.Font.SourceSans
+Toggle.Text = "Close Gui"
+Toggle.TextColor3 = Color3.fromRGB(203, 122, 49)
+Toggle.TextSize = 19.000
+Toggle.Draggable = true
+
+Toggle.MouseButton1Click:Connect(function()
+    IsOpen = not IsOpen -- สลับสถานะ
+
+    if IsOpen then
+        Toggle.Text = "Close Gui"
+    else
+        Toggle.Text = "Open Gui"
+    end
+
+    Library:ToggleUI()  -- เรียกฟังก์ชัน toggle UI ของ Library
+end)
