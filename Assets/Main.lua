87c82e4241116638fd2e6ac687b27a36329a1508
TaeBanna <sazxqinm@gmail.com>
2025-08-11 16:38:23 +0700

    Update Main.lua

--- Assets/Main.lua
+++ Assets/Main.lua
@@ -204,7 +204,8 @@ end
 --// Setup [UI]
 if (identifyexecutor) then
 	Screen = Services.Insert:LoadLocalAsset("rbxassetid://18490507748");
-	Blur = loadstring(game:HttpGet("https://raw.githubusercontent.com/lxte/lates-lib/main/Assets/Blur.lua"))();
+	Blur = loadstring(game:HttpGet("https://raw.githubusercontent.com/TaeBanna/Script/main/Assets/Blur.lua"))();
+
 else
 	Screen = (script.Parent);
 	Blur = require(script.Blur)
